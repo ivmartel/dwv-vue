e2103de6803774f0b3d78240682e111fdee3b974
ivmartel <ivmartel@gmail.com>
2018-03-21 00:04:12 +0100

    Update and fix tests.

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,9 +11,10 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.elementPresent('.legend')
+      .assert.containsText('.legend', 'Powered by dwv')
+      .assert.elementCount('.layerContainer', 1)
+      .assert.elementCount('.imageLayer', 1)
       .end()
   }
 }
--- test/unit/specs/dwv.spec.js
+++ test/unit/specs/dwv.spec.js
@@ -1,11 +1,25 @@
 import Vue from 'vue'
+import '../xhr-mock.js'
 import dwv from '@/components/dwv'
 
 describe('dwv.vue', () => {
-  it('should render correct contents', () => {
+  // Inspect the component instance on mount
+  it('sets an onClick hook when created', () => {
+    const vm = new Vue(dwv).$mount()
+    expect(typeof vm.onClick).toBe('function')
+  })
+
+  // Inspect the component instance on mount
+  it('correctly sets the legend when created', () => {
+    const vm = new Vue(dwv).$mount()
+    expect(vm.legend).toContain('Powered by dwv')
+  })
+
+  // Mount an instance and inspect the render output
+  it('renders the correct legend', () => {
     const Constructor = Vue.extend(dwv)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.legend').textContent)
-      .toEqual('Powered by dwv')
+      .toContain('Powered by dwv')
   })
 })
