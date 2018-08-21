6cb7f6a376d78c960716a5cc4e37f384528c7914
ivmartel <ivmartel@gmail.com>
2018-08-22 00:00:01 +0200

    Fix tests.

--- test/unit/specs/dwv.spec.js
+++ test/unit/specs/dwv.spec.js
@@ -12,14 +12,15 @@ describe('dwv.vue', () => {
   // Inspect the component instance on mount
   it('correctly sets the legend when created', () => {
     const vm = new Vue(dwv).$mount()
-    expect(vm.legend).toContain('Powered by dwv')
+    expect(vm.legend).toContain(
+      'Powered by <a href="https://github.com/ivmartel/dwv" title="dwv on github">dwv</a>')
   })
 
   // Mount an instance and inspect the render output
   it('renders the correct legend', () => {
     const Constructor = Vue.extend(dwv)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.legend').textContent)
-      .toContain('Powered by dwv')
+    expect(vm.$el.querySelector('.legend').textContent).toContain(
+        'Powered by <a href="https://github.com/ivmartel/dwv" title="dwv on github">dwv</a>')
   })
 })
