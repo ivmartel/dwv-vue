565a376076ef343c63f30de924bcef22e56e0aad
ivmartel <ivmartel@gmail.com>
2019-05-21 23:00:19 +0200

    Update stable deño to v0.3.0

---
+++ demo/stable/precache-manifest.0b5b7fce5e99b43a6f65462767e7eba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "dwv-vue/demo/stable/robots.txt"
+  },
+  {
+    "revision": "951b206310ea98ecda2a",
+    "url": "dwv-vue/demo/stable/js/chunk-vendors.d1ee6514.js"
+  },
+  {
+    "revision": "8a0d310e4ddd7e36bded",
+    "url": "dwv-vue/demo/stable/js/app.d990e847.js"
+  },
+  {
+    "revision": "f153f14076a37c1e9999d8436aeb5519",
+    "url": "dwv-vue/demo/stable/index.html"
+  },
+  {
+    "revision": "951b206310ea98ecda2a",
+    "url": "dwv-vue/demo/stable/css/chunk-vendors.bac6dd6c.css"
+  },
+  {
+    "revision": "8a0d310e4ddd7e36bded",
+    "url": "dwv-vue/demo/stable/css/app.6a1f5d77.css"
+  }
+];
