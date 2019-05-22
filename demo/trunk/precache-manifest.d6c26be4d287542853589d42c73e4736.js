fcbad91f41a1d45aea20c41be19ab6ad5292bef3
Travis <travis@travis-ci.org>
2019-05-22 22:32:49 +0000

    Travis build 502 pushed to gh-pages

--- demo/trunk/precache-manifest.d6c26be4d287542853589d42c73e4736.js
+++ demo/trunk/precache-manifest.d6c26be4d287542853589d42c73e4736.js
@@ -8,11 +8,11 @@ self.__precacheManifest = [
     "url": "/dwv-vue/demo/trunk/js/chunk-vendors.d1ee6514.js"
   },
   {
-    "revision": "8a0d310e4ddd7e36bded",
-    "url": "/dwv-vue/demo/trunk/js/app.d990e847.js"
+    "revision": "9e92c380db134f848698",
+    "url": "/dwv-vue/demo/trunk/js/app.d25c058d.js"
   },
   {
-    "revision": "f153f14076a37c1e9999d8436aeb5519",
+    "revision": "c38b1f381c86839b0b7e88a859469cb4",
     "url": "/dwv-vue/demo/trunk/index.html"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/dwv-vue/demo/trunk/css/chunk-vendors.bac6dd6c.css"
   },
   {
-    "revision": "8a0d310e4ddd7e36bded",
+    "revision": "9e92c380db134f848698",
     "url": "/dwv-vue/demo/trunk/css/app.6a1f5d77.css"
   }
 ];
