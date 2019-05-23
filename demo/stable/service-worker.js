b53942857c7738ec872180b7dc31cf876e12ded3
ivmartel <ivmartel@gmail.com>
2019-05-23 23:54:41 +0200

    Update stable demo to real v0.3.0.

--- demo/stable/service-worker.js
+++ demo/stable/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dwv-vue/demo/stable/precache-manifest.0b5b7fce5e99b43a6f65462767e7eba1.js"
+  "/dwv-vue/demo/stable/precache-manifest.d6c26be4d287542853589d42c73e4736.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "dwv-vue"});
