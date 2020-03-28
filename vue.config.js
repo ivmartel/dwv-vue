9dd82779f60ecb6f132b333f4c80ea039b1b305b
ivmartel <ivmartel@gmail.com>
2020-03-29 00:58:51 +0100

    Update config for stable demo.

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dwv-vue/demo/trunk/'
+    ? '/dwv-vue/demo/stable/'
     : '/',
   configureWebpack: {
     plugins: [
