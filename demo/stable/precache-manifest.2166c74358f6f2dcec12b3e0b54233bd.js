ecf42ebb2670ffa9ae630575cf36cf9acac78f43
ivmartel <ivmartel@gmail.com>
2020-03-29 01:28:07 +0100

    Update stable demo to v0.4.0.

---
+++ demo/stable/precache-manifest.2166c74358f6f2dcec12b3e0b54233bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7432f3371b729465db1",
+    "url": "/dwv-vue/demo/stable/css/app.0d8750cc.css"
+  },
+  {
+    "revision": "450a8cdda98b9621a0db",
+    "url": "/dwv-vue/demo/stable/css/chunk-vendors.7caec713.css"
+  },
+  {
+    "revision": "116d0cc2c71a28c2d73b1cbd8d1c781d",
+    "url": "/dwv-vue/demo/stable/index.html"
+  },
+  {
+    "revision": "c7432f3371b729465db1",
+    "url": "/dwv-vue/demo/stable/js/app.ed0fb288.js"
+  },
+  {
+    "revision": "450a8cdda98b9621a0db",
+    "url": "/dwv-vue/demo/stable/js/chunk-vendors.2bbd08f8.js"
+  },
+  {
+    "revision": "148a7b5c9ff9fec270911cfa1f8e8448",
+    "url": "/dwv-vue/demo/stable/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/dwv-vue/demo/stable/robots.txt"
+  }
+]);
--- demo/stable/service-worker.js
+++ demo/stable/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dwv-vue/demo/stable/precache-manifest.45aca668471841d120e2022e6147896e.js"
+  "/dwv-vue/demo/stable/precache-manifest.2166c74358f6f2dcec12b3e0b54233bd.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "dwv-vue"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "dwv-vue"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
