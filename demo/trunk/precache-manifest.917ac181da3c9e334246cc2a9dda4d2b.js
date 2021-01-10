8aa65849596cb3cd770314b8e0f5b3b40b17bf7a
Travis <travis@travis-ci.org>
2021-01-10 18:34:15 +0000

    Travis build 974 pushed to gh-pages

---
+++ demo/trunk/precache-manifest.917ac181da3c9e334246cc2a9dda4d2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9c25c81bea59a32005",
+    "url": "/dwv-vue/demo/trunk/css/app.b160b0c7.css"
+  },
+  {
+    "revision": "1c11bffb60852828fd9a",
+    "url": "/dwv-vue/demo/trunk/css/chunk-vendors.7caec713.css"
+  },
+  {
+    "revision": "c4623190109ee6ce1f4695f0d0f175d0",
+    "url": "/dwv-vue/demo/trunk/index.html"
+  },
+  {
+    "revision": "ad9c25c81bea59a32005",
+    "url": "/dwv-vue/demo/trunk/js/app.eef64cd0.js"
+  },
+  {
+    "revision": "1c11bffb60852828fd9a",
+    "url": "/dwv-vue/demo/trunk/js/chunk-vendors.98f16a51.js"
+  },
+  {
+    "revision": "148a7b5c9ff9fec270911cfa1f8e8448",
+    "url": "/dwv-vue/demo/trunk/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dwv-vue/demo/trunk/robots.txt"
+  }
+]);
