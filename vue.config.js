1b83567db978b4b6d3aa5e95e210e8cb87633c7b
ivmartel <ivmartel@gmail.com>
2021-03-26 18:29:52 +0100

    Add npm build scripts. Use them in github actions.

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/dwv-vue/demo/trunk/' : '/',
+    process.env.NODE_ENV === 'production'
+      ? '/dwv-vue/demo/' + process.env.DEPLOY_NAME + '/' : '/',
   configureWebpack: {
     plugins: [
       // copy decoders
