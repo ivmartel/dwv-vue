242c867cf85264520ce9b63d0737ab694ea67395
ivmartel <ivmartel@gmail.com>
2024-06-14 18:36:06 +0200

    First go at vue3

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
+import './style.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, md } from 'vuetify/iconsets/md'
+
 import App from './App.vue'
-import './registerServiceWorker'
 
-import 'vue-material/dist/vue-material.min.css'
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'md',
+    aliases,
+    sets: {
+      md,
+    },
+  },
+})
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).use(vuetify).mount('#app')
---
+++ vitest.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  test:{
+    globals: true,
+    environment: 'jsdom',
+    server: {
+      deps: {
+        inline: ['vuetify'],
+      },
+    },
+  }
+})
